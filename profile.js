c7478f2b8210968115183145236307c1593fb1c0
Max Carlquist <tenkaklet@gmail.com>
2021-01-16 23:06:39 +0100

    changed password begun

--- profile.js
+++ profile.js
@@ -1,18 +1,48 @@
 const calendarPage = document.querySelector('.calander-page');
+const passwordForm = document.querySelector('form');
+const authRef = firebase.auth();
+const oldPassword = document.querySelector('#old-password');
+const newPassword = document.querySelector('#new-password');
 
 calendarPage.addEventListener('click', () => {
 	window.location.href = 'calendar.html';
 });
 
-const authRef = firebase.auth();
-authRef.onAuthStateChanged(function (user) {
-	if (user) {
-		console.log('Display name onAuthStateChanged : ' + user.displayName);
-		updateUserData();
-	} else {
-		window.location.href = 'index.html';
-	}
+
+passwordForm.addEventListener('submit', e => {
+	e.preventDefault();
+
+	authRef.onAuthStateChanged(function (user) {
+		if (user) {
+			console.log('Display name onAuthStateChanged : ' + user.email);
+			updateUserData();
+			const creds = firebase.auth.EmailAuthProvider.credential(user.email, oldPassword.value);
+			user.reauthenticateWithCredential(creds)
+			.then(() => {
+				user.updatePassword(newPassword.value).then(function() {
+					// Update successful.
+					alert('success!');
+				  }).catch(function(error) {
+					console.log(error);
+				  });
+			})
+			.catch(err => {
+				console.log(err);
+			});
+
+		} else {
+			window.location.href = 'index.html';
+		}
+	});
+
 });
+
+
+
+
+
+
+
 
 function updateUserData() {
 	var userNow = firebase.auth().currentUser;
